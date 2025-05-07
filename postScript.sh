25a70c8278b5d328c6b2b1c30fa59b2f3a71bbac
Sanket Patil <sanket.n.patil@oracle.com>
2025-05-08 01:37:01 +0530

    Docker Modifications

--- postScript.sh
+++ postScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd /home/ec2-user/deploy
 
-echo "Stopping existing containers..."
-docker-compose down
+# Pull Docker images
+docker pull 309071121886.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+docker pull 309071121886.dkr.ecr.us-east-1.amazonaws.com/frontend:latest
 
-echo "Removing old images..."
-docker system prune -af
+# Run backend container
+docker run -d -p 8080:8080 309071121886.dkr.ecr.us-east-1.amazonaws.com/backend:latest
 
-echo "Starting containers..."
-docker-compose up -d
+# Run frontend container (Nginx will serve the React app)
+docker run -d -p 80:80 309071121886.dkr.ecr.us-east-1.amazonaws.com/frontend:latest
