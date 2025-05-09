bc893f22d78c6448335f3dbb4fb679144884f111
Sanket Patil <sanket.n.patil@oracle.com>
2025-05-09 13:46:23 +0530

    Docker Modifications

--- backend/src/main/java/com/example/demo/HelloController.java
+++ backend/src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/message")
     public String getMessage() {
-        return "Hii Sneh Jha from Backend !";
+        return "Hii Sneh  from Backend !";
     }
 }
